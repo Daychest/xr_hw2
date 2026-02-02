f80b0208e745e5caef61ae105612be37c341f457
Daychest <veikka.paivarinta@gmail.com>
2026-02-02 15:58:17 +0200

    added grab script

---
+++ Assets/Scripts/Grab.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Grab : MonoBehaviour
+{
+    public InputActionReference action;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        action.action.Enable();
+        action.action.performed += (ctx) =>
+        {
+            
+        };
+    }
+}
