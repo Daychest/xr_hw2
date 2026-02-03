5e9ddfa5b8af4881618b21a988b62f35b41465e7
Daychest <veikka.paivarinta@gmail.com>
2026-02-03 13:05:13 +0200

    grabPositionOffset fail

--- Assets/Scripts/Grab.cs
+++ Assets/Scripts/Grab.cs
@@ -8,11 +8,14 @@ public class Grab : MonoBehaviour
     public InputActionReference action;
     public Transform controller;
     public Transform thingToGrab;
+    Vector3 grabPositionOffset;
+    Vector3 grabRotationOffset;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        grabPositionOffset = new Vector3(0, -0.02f, 0.2f);
+        grabRotationOffset = new Vector3(90, 0, 0);
     }
 
     // Update is called once per frame
@@ -25,7 +28,7 @@ public class Grab : MonoBehaviour
         {
             thingToGrab.position = controller.position;
             thingToGrab.rotation = controller.rotation;
-            thingToGrab.Rotate(90, 0, 0);
+            thingToGrab.Rotate(grabRotationOffset);
         }
 
     }
