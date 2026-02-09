5b336f6c7c54d12d2d245e7af4c925b8d960ec9d
Daychest <veikka.paivarinta@gmail.com>
2026-02-09 13:20:45 +0200

    Magnifying glass works

--- Assets/Scripts/MagnifyingGlass.cs
+++ Assets/Scripts/MagnifyingGlass.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 
 public class MagnifyingGlass : MonoBehaviour
 {
-    public Transform mainCamera;
     public Transform magnifyingGlassCameraTransform;
     public Camera magnifyingGlassCameraCamera;
     public Transform lens;
-    public Transform lens2;
+
     // Start is called before the first frame update
     void Start()
-    {        
-        lens.transform.rotation = Quaternion.Inverse(lens.transform.rotation);
+    {
     }
 
     // Update is called once per frame
     void Update()
     {
         magnifyingGlassCameraTransform.LookAt(lens);
-        lens.LookAt(magnifyingGlassCameraTransform);
+
+        Vector3 newRotation = lens.eulerAngles;
+        newRotation.z = magnifyingGlassCameraTransform.eulerAngles.z;
+        lens.eulerAngles = newRotation;
+
         magnifyingGlassCameraCamera.nearClipPlane = Vector3.Distance(magnifyingGlassCameraTransform.position, lens.position);
     }
 }
